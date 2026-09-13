68feedcc2f762471aae3276a1c67d927de507bb5
fahmirosyadi <m.fahmi.r26@gmail.com>
2026-09-13 18:56:27 +0700

    Add debounce 500ms

--- src/app/pages/song-list-component/song-list-component.ts
+++ src/app/pages/song-list-component/song-list-component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
 import { SongService } from '../../services/song-service';
 import { Song } from '../../models/song.model';
 import { SharedModule } from '../../shared.module';
@@ -13,30 +14,57 @@ import { PaginatedComponent } from '../../components/parent-component/paginated-
 	templateUrl: './song-list-component.html',
 	styleUrl: './song-list-component.scss'
 })
-export class SongListComponent extends PaginatedComponent<Song> implements OnInit {
+export class SongListComponent
+	extends PaginatedComponent<Song>
+	implements OnInit, OnDestroy {
 
-  constructor(
+	private searchSubject = new Subject<string>();
+	private destroy$ = new Subject<void>();
+
+	constructor(
 		private service: SongService,
 		private router: Router
 	) {
-    super();
-  }
-
-	async ngOnInit() {
-    await this.loadData();
+		super();
 	}
 
-  protected override fetchData(): Promise<{ data: Song[]; total: number; }> {
-    return this.service.getPaged(this.page, this.pageSize, this.search, 1);
-  }
+	async ngOnInit() {
+		this.searchSubject
+			.pipe(
+				debounceTime(500),
+				distinctUntilChanged(),
+				takeUntil(this.destroy$)
+			)
+			.subscribe(() => {
+				this.page = 0;
+				this.loadData();
+			});
+
+		await this.loadData();
+	}
+
+	onSearch2(value: string) {
+		this.searchSubject.next(value);
+	}
+
+	protected override fetchData(): Promise<{ data: Song[]; total: number }> {
+		return this.service.getPaged(
+			this.page,
+			this.pageSize,
+			this.search,
+			1
+		);
+	}
 
 	viewSong(song: Song) {
-
 		this.router.navigate([
 			'/song-view',
 			song.id
 		]);
-
 	}
 
+	ngOnDestroy() {
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
 }
